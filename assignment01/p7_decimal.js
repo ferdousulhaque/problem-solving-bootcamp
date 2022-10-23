ed65042d5e0c33375140d390c7af766ceb54e6e5
A S Md Ferdousul Haque <ferdousul.haque@gmail.com>
2022-10-23 07:57:21 +0600

    Adding the Decimal Solution

--- assignment01/p7_decimal.js
+++ assignment01/p7_decimal.js
@@ -6,9 +6,33 @@
 const isPalindrome = function(x) {
     if(x < 0)
         return false;
-
+    else if(x.length == 1)
+        return true;
+    else{
+        let rx = reverseByDecimalPlace(x);
+        if(rx == x){
+            return true;
+        }
+    }
     
+    return false;
 };
+
+const reverseByDecimalPlace = (number) => {
+    let next = number % 10;
+    let n = parseInt(number / 10);
+    let rvs = next;
+
+    while(n > 0){
+        next = n % 10;
+        n = parseInt(n / 10);
+        rvs = next + 10 * rvs;
+    }
+
+    return rvs;
+}
+
+// reverseByDecimalPlace(123);
 
 // Test Case 1
 console.log(isPalindrome(-121));
