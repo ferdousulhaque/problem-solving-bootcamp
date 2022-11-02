dd7fe2721ce6b9d56981c0c12b2e074e1114443a
A S Md Ferdousul Haque <ferdousul.haque@gmail.com>
2022-11-02 06:49:00 +0600

    Assignment 02 Complete

--- assignment02/p12.js
+++ assignment02/p12.js
@@ -1,17 +1,29 @@
 // https://www.hackerrank.com/challenges/drawing-book/problem
 
 const pageCount = (n, p) => {
-    
-
+    let pageFromFirst = parseInt(p / 2);
+    if(n % 2 == 0)
+        n++;
+    let pageFromEnd = parseInt(Math.abs(p - n)/2);
+    return pageFromFirst < pageFromEnd ? pageFromFirst : pageFromEnd;
 }
 
 // Test Case 1
-console.log(pageCount(3, [1, 2, 3, 4, 5]));
+console.log(pageCount(12, 7));
 
 // Test Case 2
-// console.log(pageCount(1, [2]));
+console.log(pageCount(6, 2));
 
 // Test Case 3
-// console.log(pageCount(2, [1, 1000000]));
+console.log(pageCount(6, 4));
+
+// Test Case 4
+console.log(pageCount(5, 3));
+
+// Test Case 5
+console.log(pageCount(5, 2));
+
+// Test Case 6
+console.log(pageCount(10000, 1001));
 
 
