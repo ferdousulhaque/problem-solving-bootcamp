7ff472dd238358ffa42d5f8a8768fc68b8345926
A S Md Ferdousul Haque <ferdousul.haque@gmail.com>
2025-04-07 18:26:11 +0600

    Adding Solution

--- subeen/remove-duplicates-in-array.py
+++ subeen/remove-duplicates-in-array.py
@@ -6,7 +6,12 @@
 def code(**kwargs) -> list:
     # Change the below variables according to your problem
     arr = kwargs['arr']
-    return [1,2,3]
+    cursor = 0
+    for i in range(len(arr)):
+        if arr[cursor] != arr[i]:
+            cursor += 1
+            arr[cursor] = arr[i]
+    return arr[:cursor+1]
 
 
 ## Reserved for Test Cases
@@ -15,14 +20,26 @@ cases = [
         "input": code(arr=[1,1,2,2,2,3,3]),
         "expected": [1,2,3]
     },
+    {
+        "input": code(arr=[5,5,3,3,3,2,1]),
+        "expected": [5,3,2,1]
+    },
+    {
+        "input": code(arr=[1,1,1,1]),
+        "expected": [1]
+    },
+    {
+        "input": code(arr=[]),
+        "expected": []
+    }
 ]
 
 def tests(cases: list) -> None:
     # print()
-    for case in cases:
+    for idx, case in enumerate(cases):
         if case['input'] == case['expected']:
-            print("Case: PASS")
+            print(f'Case {idx+1}: PASS')
         else:
-            print("Case: FAIL")
+            print(f'Case {idx+1}: FAIL')
 
 tests(cases)
