4fd2e8e25ceecb8260251f9b3fb325b27aee68fb
A S Md Ferdousul Haque <ferdousul.haque@gmail.com>
2025-04-23 07:12:45 +0600

    Added New Solutions

--- subeen/maximum-unsorted-subarray.py
+++ subeen/maximum-unsorted-subarray.py
@@ -6,21 +6,45 @@
 def code(**kwargs) -> list:
     # Change the below variables according to your problem
     A = kwargs['A']
-    A_sort = A[0:]
-    A_sort.sort()
-    output = []
-    last_unmatch = 0
-    for idx in range(len(A)):
-        if(A[idx] != A_sort[idx] and len(output) == 0):
-            output.append(idx)
-        if(A[idx] != A_sort[idx]):
-            last_unmatch = idx
     
-    if(len(output) == 0):
-        return [-1]
+    # T -> O(nlogn) and S -> O(n) Solution
+    # A_sort = A[0:]
+    # A_sort.sort()
+    # output = []
+    # last_unmatch = 0
+    # for idx in range(len(A)):
+    #     if(A[idx] != A_sort[idx] and len(output) == 0):
+    #         output.append(idx)
+    #     if(A[idx] != A_sort[idx]):
+    #         last_unmatch = idx
+    
+    # if(len(output) == 0):
+    #     return [-1]
 
-    output.append(last_unmatch)
-    return output
+    # output.append(last_unmatch)
+    # return output
+    
+    # T -> O(n) and S -> O(1) Solution
+    n = len(A)
+    start, end = -1, -1
+    min_val, max_val = float('inf'), float('-inf')
+
+    for i in range(n):
+        if A[i] < max_val:
+            end = i
+        else:
+            max_val = max(max_val, A[i])
+        
+        if A[n-i-1] > min_val:
+            start = n-i-1
+        else:
+            min_val = min(min_val, A[n-i-1])
+
+    if start == -1:
+        return [-1]
+    
+    return [start, end]
+    
 
 
 ## Reserved for Test Cases
@@ -33,22 +57,22 @@ cases = [
         "input": code(A=[1,4,2,3,5]),
         "expected": [1,3]
     },
-    {                
-        "input": code(A=[1,2,3,4,5]),
-        "expected": [-1]
-    },
-    {                
-        "input": code(A=[3,2,1]),
-        "expected": [0,2]
-    },
-    {                
-        "input": code(A=[5,6,1,2,4,7]),
-        "expected": [0,4]
-    },
-    {                
-        "input": code(A=[-100,4,7,-110,10,50]),
-        "expected": [0,3]
-    },
+    # {                
+    #     "input": code(A=[1,2,3,4,5]),
+    #     "expected": [-1]
+    # },
+    # {                
+    #     "input": code(A=[3,2,1]),
+    #     "expected": [0,2]
+    # },
+    # {                
+    #     "input": code(A=[5,6,1,2,4,7]),
+    #     "expected": [0,4]
+    # },
+    # {                
+    #     "input": code(A=[-100,4,7,-110,10,50]),
+    #     "expected": [0,3]
+    # },
 ]
 
 def tests(cases: list) -> None:
